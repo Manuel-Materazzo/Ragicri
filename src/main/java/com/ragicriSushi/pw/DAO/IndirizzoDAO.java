29c85654d864e29cc557993f61ae2ea33a286468
Giacomo cortese <giacomo.cortese@stud.tecnicosuperiorekennedy.it>
2021-12-28 11:49:56 +0100

    implementazione priova my sql

--- src/main/java/com/ragicriSushi/pw/DAO/IndirizzoDAO.java
+++ src/main/java/com/ragicriSushi/pw/DAO/IndirizzoDAO.java
@@ -1,4 +1,15 @@
 package com.ragicriSushi.pw.DAO;
 
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.UUID;
+
+
 public class IndirizzoDAO {
+
+
 }
--- src/main/java/com/ragicriSushi/pw/DAO/UtenteDAO.java
+++ src/main/java/com/ragicriSushi/pw/DAO/UtenteDAO.java
@@ -1,4 +1,22 @@
 package com.ragicriSushi.pw.DAO;
 
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@Table(name = "UtenteDAO")
 public class UtenteDAO {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
 }
