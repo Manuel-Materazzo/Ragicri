c490624bf7cf938f4979a7ed35d86cde627919a4
CristianTomasi <cristian.tomasi@stud.tecnicosuperiorekennedy.it>
2022-01-14 16:31:35 +0100

    Aggiunto servizio modifica e getById su IndirizzoService e IndirizzoController.

--- src/main/java/com/ragicriSushi/pw/Controller/IndirizzoController.java
+++ src/main/java/com/ragicriSushi/pw/Controller/IndirizzoController.java
@@ -1,14 +1,15 @@
 package com.ragicriSushi.pw.Controller;
 
+import com.ragicriSushi.pw.DTO.IndirizzoDTO;
+import com.ragicriSushi.pw.DTO.UtenteDTO;
 import com.ragicriSushi.pw.Repository.IndirizzoRepository;
 import com.ragicriSushi.pw.Service.IndirizzoService;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(
@@ -24,5 +25,29 @@ public class IndirizzoController {
     @ApiOperation("Ritorna tutti gli indirizzi")
     public ResponseEntity<Object> getAll(){
         return ResponseEntity.ok(indirizzoService.getAll());
+    }
+
+    @GetMapping(path = "/{id}")
+    @ApiOperation("Ritorna un indirizzo con l'id inserito")
+    public ResponseEntity<Object> getById(@PathVariable int id) {
+        IndirizzoDTO dto = indirizzoService.getById(id);
+
+        if (dto == null) {
+            return ResponseEntity.notFound().build();
+        } else {
+            return ResponseEntity.ok(dto);
+        }
+    }
+
+    @PostMapping(path = "/update")
+    @ApiOperation("Aggiorna un indirizzo")
+    public ResponseEntity<Object> update(@RequestBody IndirizzoDTO dto){
+        IndirizzoDTO result = indirizzoService.update(dto);
+        if (dto == null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"status\": \"Indirizzo non trovato.\"}");
+        }
+        else {
+            return ResponseEntity.ok(dto);
+        }
     }
 }
