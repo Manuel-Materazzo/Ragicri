f07f658a0dc058832ef9e47a09e4eeb2b1212c5f
CristianTomasi <cristian.tomasi@stud.tecnicosuperiorekennedy.it>
2022-01-21 15:57:56 +0100

    Modificato html del login e pulito codice sul service

--- src/app/Pages/UserPages/login-boxed/login-boxed.component.ts
+++ src/app/Pages/UserPages/login-boxed/login-boxed.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { UtenteService } from 'src/app/Theme/Utente/utente.service';
 
 @Component({
   selector: 'app-login-boxed',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginBoxedComponent implements OnInit {
 
-  constructor() { }
+  username: String;
+  password: String;
+
+
+  constructor(private router: Router, private ordinazioneService: UtenteService, private modalService: NgbModal) {
+  }
 
   ngOnInit() {
   }
+
+  
 
 }
