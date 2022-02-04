53d54f1e6118459b1be9c96fa64dccef2d9948e7
Radu.Sclifos <radu.sclifos@stud.tecnicosuperiorekennedy.it>
2022-02-04 12:53:23 +0100

    Fix sidebar

--- src/app/Layout/Components/sidebar/sidebar.component.ts
+++ src/app/Layout/Components/sidebar/sidebar.component.ts
@@ -7,6 +7,7 @@ import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
 
